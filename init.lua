662d50865273d13ff325b3d7706ea5469977e0c4
nenych <nenych.m@gmail.com>
2024-05-20 23:08:39 +0200

    Update plugins

--- init.lua
+++ init.lua
@@ -7,3 +7,13 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- Ensure there's always an empty line at the end of the file
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--pattern = "*",
+--callback = function()
+--if vim.fn.getline(vim.fn.line("$")) ~= "" then
+--vim.fn.append(vim.fn.line("$"), "")
+--end
+--end,
+--})
