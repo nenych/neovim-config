b34942e46746402c9326d70cc33175ab038c3045
nenych <nenych.m@gmail.com>
2024-05-23 23:39:18 +0200

    Update the gitsigns configuration

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,21 @@
+--return {
+--"christoomey/vim-tmux-navigator",
+--config = function()
+--vim.g.tmux_navigator_no_mappings = 1
+--vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>")
+--vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>")
+--vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>")
+--vim.keymap.set("n", "C-l", ":TmuxNavigateRight<CR>")
+--end,
+--}
+
+return {
+  "christoomey/vim-tmux-navigator",
+  --config = function()
+  --vim.g.tmux_navigator_no_mappings = 1
+  --vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>")
+  --vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>")
+  --vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>")
+  --vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>")
+  --end,
+}
